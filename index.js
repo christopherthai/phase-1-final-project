0fa85f68f43843fcf348246987e42365f26134c9
Christopher Thai <christopherthai.nyli@gmail.com>
2024-03-04 23:44:54 -0500

    Create getWeatherCode() function to get the weather code from the weather api

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ const checkWhiteSpace = (str) => {
   return /\s/.test(str)
 }
 
-// Calcualte the latitude and longitude coordinates from the Weather API
-const calculateCoordinates = (citys, states, countrys) => {
+// Get the latitude and longitude coordinates from the Weather API
+const getCoordinates = (city, state, country) => {
   
-  // Replace the white space with a '+' symbol in the city name
-  // if(checkWhiteSpace(city)){
-  //   let cityName = city.split(' ').join('+') 
-  // }
-
-  
-  let state = "New York"
-  let city = "New York"
-  let country = "United States"
-  let cityName = city.split(' ').join('+') 
+  // let state = "New York"
+  // let city = "New York"
+  // let country = "United States"
+  // let cityName = city.split(' ').join('+') 
+        
+  //Replace the white space with a '+' symbol in the city name
+  if(checkWhiteSpace(city)){
+    let cityName = city.split(' ').join('+') 
+  }
 
   fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=10&language=en`)
   .then(response => response.json())
@@ -27,12 +26,12 @@ const calculateCoordinates = (citys, states, countrys) => {
       // Get the latitude and longitude of the location is in the United States
       if ((data.name === city) && (data.admin1 === state)){
 
-        calculateWeatherCode(data.latitude, data.longitude)
+        getWeatherCode(data.latitude, data.longitude)
 
       // Get the latitude and longitude of the location outside of the United States
       } else if ((data.name === city) && (data.country === country) && (data.country_code !== "US")){
 
-        calculateWeatherCode(data.latitude, data.longitude)
+        getWeatherCode(data.latitude, data.longitude)
 
       }
       
@@ -40,6 +39,13 @@ const calculateCoordinates = (citys, states, countrys) => {
   })
 }
 
-const calculateWeatherCode = (latitude, longitude) => {
+// Get weather code base on the latitude and longitude from the weather api
+const getWeatherCode = (latitude, longitude) => {
   
+  fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=weather_code&temperature_unit=fahrenheit&forecast_days=1`)
+  .then(response => response.json())
+  .then(weather_data => {
+    console.log(weather_data.current.weather_code)
+    }
+  )
 }
